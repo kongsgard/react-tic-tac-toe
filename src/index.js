7cbdea069285989840d7da376c5242784945ff9e
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2018-01-03 12:14:56 -0800

    Display the location for each move in the format (col, row) in the move history list

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ class Game extends React.Component {
     this.state = {
       history: [{
         squares: Array(9).fill(null),
+        moveLocation: null,
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -66,6 +67,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        moveLocation: i,
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -86,7 +88,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + ' (' + (step.moveLocation%3+1) + ', ' + (Math.floor(step.moveLocation/3)+1) + ')' :
         'Go to game start';
       return (
         <li key={move}>
