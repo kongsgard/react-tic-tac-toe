a47bd26a60e5f65f1b570d43e633e1e46b081e5c
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2018-01-03 21:02:10 -0800

    Add a toggle button that lets you sort the moves in either ascending or descending order

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ class Game extends React.Component {
       }],
       stepNumber: 0,
       xIsNext: true,
+      sortMovesAscending: true,
     }
   }
 
@@ -96,6 +97,9 @@ class Game extends React.Component {
         </li>
       );
     });
+    if (this.state.sortMovesAscending === false) {
+      moves.sort((a, b) => { return b.key - a.key; });
+    }
 
     let status;
     if (winner) {
@@ -116,6 +120,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <button onClick={() => this.setState({sortMovesAscending: !this.state.sortMovesAscending})}>Reverse moves</button>
           <ol>{moves}</ol>
         </div>
       </div>
